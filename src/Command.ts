3feca55bfeea9dc70b51d44c780854d88e52b8d2
partsci <97770763+partsci@users.noreply.github.com>
2022-04-02 11:01:38 -0700

    Update Command.ts

--- src/Command.ts
+++ src/Command.ts
@@ -57,7 +57,7 @@ export abstract class Command {
    *  @param {message} message that contains granted permissions
    */
    public hasChannelPermissions(message: Message) {
-      return message.channel.permissionsFor(message.guild?.me).has('SEND_MESSAGES')
+    return nessage?.guild?.me?.permissionsIn(message.channel).has('SEND_MESSAGES');
    }
 
   /**
